43e2248cf67567fe211aed549393d443327fbf5f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-19 19:32:17 +0000

    Deploy remyguillaumot/projet-frontend to github.com/remyguillaumot/projet-frontend.git:gh-pages

--- main.bundle.js
+++ main.bundle.js
@@ -69,14 +69,22 @@ var AppComponent = /** @class */ (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("./src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__meteo_meteo_component__ = __webpack_require__("./src/app/meteo/meteo.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser_animations__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/animations.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_common_http__ = __webpack_require__("./node_modules/@angular/common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__meteo_meteo_component__ = __webpack_require__("./src/app/meteo/meteo.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__angular_material_card__ = __webpack_require__("./node_modules/@angular/material/esm5/card.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__angular_material_button__ = __webpack_require__("./node_modules/@angular/material/esm5/button.es5.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
+
+
+
 
 
 
@@ -87,14 +95,18 @@ var AppModule = /** @class */ (function () {
     AppModule = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
             declarations: [
-                __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
-                __WEBPACK_IMPORTED_MODULE_3__meteo_meteo_component__["a" /* MeteoComponent */]
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__meteo_meteo_component__["a" /* MeteoComponent */]
             ],
             imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser_animations__["a" /* BrowserAnimationsModule */],
+                __WEBPACK_IMPORTED_MODULE_6__angular_material_card__["a" /* MatCardModule */],
+                __WEBPACK_IMPORTED_MODULE_7__angular_material_button__["a" /* MatButtonModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_common_http__["b" /* HttpClientModule */]
             ],
             providers: [],
-            bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
         })
     ], AppModule);
     return AppModule;
@@ -104,26 +116,14 @@ var AppModule = /** @class */ (function () {
 
 /***/ }),
 
-/***/ "./src/app/meteo/meteo.component.css":
-/***/ (function(module, exports) {
-
-module.exports = ""
-
-/***/ }),
-
-/***/ "./src/app/meteo/meteo.component.html":
-/***/ (function(module, exports) {
-
-module.exports = "<p>\n  {{ town.icon }}\n  {{ town.temperature }}\n</p>"
-
-/***/ }),
-
-/***/ "./src/app/meteo/meteo.component.ts":
+/***/ "./src/app/meteo.service.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MeteoComponent; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MeteoService; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common_http__ = __webpack_require__("./node_modules/@angular/common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_catch__ = __webpack_require__("./node_modules/rxjs/_esm5/add/operator/catch.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -134,21 +134,104 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
+
+
+var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_common_http__["c" /* HttpHeaders */]({ 'Content-Type': 'application/json' });
+var URL = "https://projet-pierrat.herokuapp.com";
+var MeteoService = /** @class */ (function () {
+    function MeteoService(http) {
+        this.http = http;
+    }
+    MeteoService.prototype.GetAllTowns = function () {
+        return this.http.get(URL + '/towns.json');
+    };
+    MeteoService.prototype.GetTownById = function (id) {
+        return this.http.get(URL + '/towns/' + id + '.json');
+    };
+    MeteoService.prototype.UpdateTown = function (town) {
+        return this.http.put(URL + '/towns/' + town.id + '.json', town, { headers: headers });
+    };
+    MeteoService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_common_http__["a" /* HttpClient */]])
+    ], MeteoService);
+    return MeteoService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/meteo/meteo.component.css":
+/***/ (function(module, exports) {
+
+module.exports = "p{\n  font-size: 3em;\n}"
+
+/***/ }),
+
+/***/ "./src/app/meteo/meteo.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\n<div class=\"card\">\n  <div class=\"card-content\">\n    <p>{{city.name}}\n      <i class=\"wi fa-3x  wi-forecast-io-{{city.icon}}\"></i>\n      {{city.temperature}}°C\n    </p>\n    <div class=\"card-content\">\n    </div>\n  </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/meteo/meteo.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MeteoComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__town__ = __webpack_require__("./src/app/town.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__meteo_service__ = __webpack_require__("./src/app/meteo.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
 var MeteoComponent = /** @class */ (function () {
-    function MeteoComponent() {
-        this.town = { id: 1, name: 'belfort', temperature: 0, icon: "rain" };
+    function MeteoComponent(MeteoService) {
+        this.MeteoService = MeteoService;
+        this.city = new __WEBPACK_IMPORTED_MODULE_1__town__["a" /* Town */]();
     }
     MeteoComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.MeteoService.GetTownById(4).subscribe(function (town) { return _this.city = town; });
     };
     MeteoComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
             selector: 'app-meteo',
             template: __webpack_require__("./src/app/meteo/meteo.component.html"),
-            styles: [__webpack_require__("./src/app/meteo/meteo.component.css")]
+            styles: [__webpack_require__("./src/app/meteo/meteo.component.css")],
+            providers: [__WEBPACK_IMPORTED_MODULE_2__meteo_service__["a" /* MeteoService */]]
         }),
-        __metadata("design:paramtypes", [])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__meteo_service__["a" /* MeteoService */]])
     ], MeteoComponent);
     return MeteoComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/town.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Town; });
+var Town = /** @class */ (function () {
+    function Town(values) {
+        if (values === void 0) { values = {}; }
+        Object.assign(this, values);
+    }
+    return Town;
 }());
 
 
@@ -181,7 +264,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* enableProdMode */])();
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* enableProdMode */])();
 }
 Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
     .catch(function (err) { return console.log(err); });
